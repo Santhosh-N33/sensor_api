4e6c322965e02ce615fdcfee3e5a3bac523eee39
Santhosh N <129830790+Santhosh-N33@users.noreply.github.com>
2025-05-04 18:44:28 +0530

    Update firebase_admin_setup.py

--- firebase_admin_setup.py
+++ firebase_admin_setup.py
@@ -1,8 +1,13 @@
-# firebase_admin_setup.py
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+# Load Firebase credentials from Render's Secrets Manager
+service_account_key = json.loads(os.getenv("FIREBASE_SERVICE_ACCOUNT"))
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate(service_account_key)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
