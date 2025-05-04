d306f065f3c133f35eb8deeecfd7f8340cc2e7e8
Santhosh N <129830790+Santhosh-N33@users.noreply.github.com>
2025-05-04 18:14:40 +0530

    secrate

---
+++ firebase_admin_setup.py
@@ -0,0 +1,8 @@
+# firebase_admin_setup.py
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
