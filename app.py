94a13a116ad80dbe383306dccffa19d6e78d7408
Santhosh N <129830790+Santhosh-N33@users.noreply.github.com>
2025-05-04 15:37:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ def upload_sensor():
 
     return jsonify({"status": "success"}), 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
